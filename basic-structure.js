569d2ebf081e30403483ec805ec839a2f523a9d8
Alex Curran <alexcurran.va@gmail.com>
2021-12-15 17:17:33 -0600

    Base linked list node class

---
+++ basic-structure.js
@@ -0,0 +1,13 @@
+"use strict";
+
+class ListNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+const a = new ListNode("A");
+const b = new ListNode("B");
+const c = new ListNode("C");
+const d = new ListNode("D");
