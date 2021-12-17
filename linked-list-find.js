0dfe162da16b8684aabfb2a1d7825881ac2b0c59
Alex Curran <alexcurran.va@gmail.com>
2021-12-17 10:51:12 -0600

    Recursive and iterative solutions for linked-list-find.js

---
+++ linked-list-find.js
@@ -0,0 +1,45 @@
+"use strict";
+
+class ListNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+const a = new ListNode("A");
+const b = new ListNode("B");
+const c = new ListNode("C");
+const d = new ListNode("D");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+// // Iterative solution
+// // Time: O(n)
+// // Space: O(1)
+// function linkedListFind(head, target) {
+//     let current = head;
+
+//     while (current !== null) {
+//         if (current.val === target) return true;
+
+//         current = current.next; // step to next node
+//     }
+
+//     return false;
+// }
+
+// Recursive solution
+// Time: O(n)
+// Space: O(n)
+function linkedListFind(head, target) {
+    if (head === null) return false;
+
+    if (head.val === target) return true;
+
+    return linkedListFind(head.next, target);
+}
+
+console.log( linkedListFind(a, "C") ); // true
