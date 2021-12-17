4b1c6c0eae9d2b194578b40045e34291ced10aac
Alex Curran <alexcurran.va@gmail.com>
2021-12-17 10:36:03 -0600

    Iteration and recursion solutions for sum-list.js

---
+++ sum-list.js
@@ -0,0 +1,46 @@
+"use strict";
+
+class ListNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+const a = new ListNode(2);
+const b = new ListNode(8);
+const c = new ListNode(3);
+const d = new ListNode(-1);
+const e = new ListNode(7);
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+// // Iterative solution
+// // Time: O(n)
+// // Space: O(1)
+// function sumList(head) {
+//     let sum = 0;
+//     let current = head;
+
+//     while (current !== null) {
+//         sum += current.val;
+//         current = current.next; // Step to next node
+//     }
+
+//     return sum;
+// }
+
+// Recursive solution
+// Time: O(n)
+// Space: O(n)
+function sumList(head) {
+    // base case
+    if (head === null) return 0;
+
+    return head.val + sumList(head.next);
+}
+
+console.log( sumList(a) ); // 19
